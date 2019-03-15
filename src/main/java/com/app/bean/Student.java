5e2e11b1be0ae80f3e6a9891ac94dd750394052e
Gurpreet <gurpreet.khanuja01@gmail.com>
2019-03-15 11:27:16 -0400

    Changes in Student.java

--- src/main/java/com/app/bean/Student.java
+++ src/main/java/com/app/bean/Student.java
@@ -32,16 +32,7 @@ public class Student {
 	public void setGender(Gender gender) {
 		this.gender = gender;
 	}
-<<<<<<< .mine
 
-	public String getEmailAddress() {
-		return emailAddress;
-	}
-
-	public void setEmailAddress(String emailAddress) {
-		this.emailAddress = emailAddress;
-	}
-=======
 
 	public String getEmail() {
 		return email;
@@ -50,14 +41,4 @@ public class Student {
 	public void setEmail(String email) {
 		this.email = email;
 	}
->>>>>>> .theirs
-	
-	
-<<<<<<< .mine
-	
-
-=======
-	
-	
->>>>>>> .theirs
 }
